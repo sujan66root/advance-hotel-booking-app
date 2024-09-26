15c3cdbf2ec7a99491e2118bdec421e730202cb5
Sujan Gautam <sujan.rock45@gmail.com>
2024-09-25 23:27:04 -0400

    generated booking confirmation code

---
+++ backend/src/main/java/com/sujan/hotelbooking/utils/ConfirmationCode.java
@@ -0,0 +1,19 @@
+package com.sujan.hotelbooking.utils;
+
+import java.security.SecureRandom;
+
+public class ConfirmationCode {
+    private static final String ALPHANUMERIC_STRING = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final SecureRandom secureRandom = new SecureRandom();
+
+
+    public static String generateRandomConfirmationCode(int length) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            int randomIndex = secureRandom.nextInt(ALPHANUMERIC_STRING.length());
+            char randomChar = ALPHANUMERIC_STRING.charAt(randomIndex);
+            stringBuilder.append(randomChar);
+        }
+        return stringBuilder.toString();
+    }
+}
