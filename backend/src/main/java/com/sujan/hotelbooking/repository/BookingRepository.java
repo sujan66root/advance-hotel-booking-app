60ef4e59faa124d922d06eb0fd2a83e9898cfd08
Sujan Gautam <sujan.rock45@gmail.com>
2024-09-27 00:39:03 -0400

    updated repositories

--- backend/src/main/java/com/sujan/hotelbooking/repository/BookingRepository.java
+++ backend/src/main/java/com/sujan/hotelbooking/repository/BookingRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
-    Optional<Booking> bookingConfirmationCode(String confirmationCode);
+    Optional<Booking> findByBookingConfirmationCode(String confirmationCode);
 }
--- backend/src/main/java/com/sujan/hotelbooking/repository/UserRepository.java
+++ backend/src/main/java/com/sujan/hotelbooking/repository/UserRepository.java
@@ -6,5 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<Users, Long> {
+    // This method checks if a user exists with the given email.
+    boolean existsByEmail(String email);
+
+    // This method retrieves a user by email.
     Optional<Users> findByEmail(String email);
 }
