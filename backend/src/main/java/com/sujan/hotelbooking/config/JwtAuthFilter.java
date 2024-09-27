dc69f324300a679b7791b9326a30c84615f9c62c
Sujan Gautam <sujan.rock45@gmail.com>
2024-09-27 00:43:26 -0400

    added missing annotation in filter

--- backend/src/main/java/com/sujan/hotelbooking/config/JwtAuthFilter.java
+++ backend/src/main/java/com/sujan/hotelbooking/config/JwtAuthFilter.java
@@ -10,11 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
+@Component
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     @Autowired
